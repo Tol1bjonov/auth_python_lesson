1aa048cc46af7d777421358616d1d507b7789971
Yusufjon Tolibjonov <yusufjontolibjonov3@gmail.com>
2025-03-14 12:39:30 +0500

    feat: list comprehension

--- 9thlesson.py
+++ 9thlesson.py
@@ -32,80 +32,142 @@
 
 #3
 
-class Car:
-    def __init__(self, brand, model, year):
-        self.brand = brand
-        self.model = model
-        self.year = year
+# class Car:
+#     def __init__(self, brand, model, year):
+#         self.brand = brand
+#         self.model = model
+#         self.year = year
+#
+#     def __eq__(self, other):
+#         if isinstance(other, Car):
+#             return (self.brand, self.model, self.year) == (other.brand, other.model, other.year)
+#         return False
+#
+#     def __hash__(self):
+#         return hash((self.brand, self.model, self.year))
+#
+# # Testing in a set
+# car1 = Car("Toyota", "Corolla", 2020)
+# car2 = Car("Toyota", "Corolla", 2020)
+# car3 = Car("Honda", "Civic", 2021)
+#
+# car_set = {car1, car2, car3}
+# print(len(car_set))  # Output: 2 (car1 and car2 are considered the same)
+#
+# #3
+# mutable_key = { "key": "value" }
+#
+# try:
+#     my_dict = {mutable_key: "test"}
+# except TypeError as e:
+#     print(f"Error: {e}")
+#
+# #4
+# class Point:
+#     def __init__(self, x, y):
+#         self.x = x
+#         self.y = y
+#
+#     def __eq__(self, other):
+#         return isinstance(other, Point) and (self.x, self.y) == (other.x, other.y)
+#
+#     def __hash__(self):
+#         return hash(self.x + self.y)
+#
+# # Testing hash conflict
+# p1 = Point(1, 2)
+# p2 = Point(2, 1)
+# p3 = Point(3, 0)
+#
+# point_set = {p1, p2, p3}
+# print(len(point_set))
+#
+# #4
+# class HashableObject:
+#     def __init__(self, value):
+#         self.value = value
+#
+#     def __hash__(self):
+#         return hash(self.value)
+#
+# class NonHashableObject:
+#     def __init__(self, value):
+#         self.value = value
+#
+# hashable = HashableObject(10)
+# non_hashable = NonHashableObject(20)
+#
+# my_set = {hashable}
+#
+# try:
+#     my_set.add(non_hashable)
+# except TypeError as e:
+#     print(f"Error: {e}")
+# #5
+# key1 = frozenset([1, 2, 3])
+# key2 = frozenset([4, 5, 6])
+#
+# my_dict = {key1: "Group A", key2: "Group B"}
+# print(my_dict)
 
-    def __eq__(self, other):
-        if isinstance(other, Car):
-            return (self.brand, self.model, self.year) == (other.brand, other.model, other.year)
-        return False
+max_num = 0
+def find_max(a,b,c):
+    if a>b and a>c:
+        return a
+    elif b>a and b>c:
+        return b
+    else:
+        return c
+print(f'the biggest number is {find_max(11,22,3)}')
 
-    def __hash__(self):
-        return hash((self.brand, self.model, self.year))
+# def factorial_cal():
+#     i = 1
+#     factorial = 1
+#     n = int(input('enter a number: '))
+#     while i <= n:
+#         factorial = factorial * i
+#         i = i + 1
+#     return factorial
+# print(f'{factorial_cal()}')
 
-# Testing in a set
-car1 = Car("Toyota", "Corolla", 2020)
-car2 = Car("Toyota", "Corolla", 2020)
-car3 = Car("Honda", "Civic", 2021)
+# def factorial_call():
+#     i=1
+#     factorial =1
+#     n = int(input('enter the number: '))
+#     while i <= n:
+#         factorial = factorial*i
+#         i = i+1
+#     return factorial
+# print(factorial_call())
 
-car_set = {car1, car2, car3}
-print(len(car_set))  # Output: 2 (car1 and car2 are considered the same)
 
-#3
-mutable_key = { "key": "value" }
+def LinearSearch(arr, target):
 
-try:
-    my_dict = {mutable_key: "test"}
-except TypeError as e:
-    print(f"Error: {e}")
+        for i in range(len(arr)):
 
-#4
-class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+            if arr[i] == target:
 
-    def __eq__(self, other):
-        return isinstance(other, Point) and (self.x, self.y) == (other.x, other.y)
+             return i
 
-    def __hash__(self):
-        return hash(self.x + self.y)
 
-# Testing hash conflict
-p1 = Point(1, 2)
-p2 = Point(2, 1)
-p3 = Point(3, 0)
+        return -1
 
-point_set = {p1, p2, p3}
-print(len(point_set))
 
-#4
-class HashableObject:
-    def __init__(self, value):
-        self.value = value
+myList = [1,3,4,6,7,8,10,12,23,45,56,78,99]
 
-    def __hash__(self):
-        return hash(self.value)
+target = 7
 
-class NonHashableObject:
-    def __init__(self, value):
-        self.value = value
+index = LinearSearch(myList, target)
 
-hashable = HashableObject(10)
-non_hashable = NonHashableObject(20)
 
-my_set = {hashable}
+if index != -1:
+    print(target, "was found at index", index)
 
-try:
-    my_set.add(non_hashable)
-except TypeError as e:
-    print(f"Error: {e}")
-#5
-key1 = frozenset([1, 2, 3])
-key2 = frozenset([4, 5, 6])
+else:
+    print(target, "was not found")
 
-my_dict = {key1: "Group A", key2: "Group B"}
-print(my_dict)
+
+
+
+
+
