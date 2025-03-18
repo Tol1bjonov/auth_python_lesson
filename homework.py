417e073562fb900d72cfb6d25dcd8e9799f42f7c
Yusufjon Tolibjonov <yusufjontolibjonov3@gmail.com>
2025-03-18 15:29:16 +0500

    homework added

---
+++ homework.py
@@ -0,0 +1,37 @@
+#Task 1
+
+# lst =[1, 12, 13, 14, 15, 2, 3]
+# result = [num for num in lst if num > 5 and num % 5== 0]
+# print(result)
+
+
+#Task 2
+
+# names = ["Ramzan", "Ali", "Ahmed"]
+# ages = [29, 30, 35]
+# name_age_dick = {names[i]: ages[i] for i in range(len(names))}
+# print(name_age_dick)
+
+
+#Task 3
+
+# dict1 = {'key1':4, 'key2':5, 'key3':3, 'key4':9, 'key5':8}
+# doubled_dict = {key: value * 2 for key, value in dict1.items()}
+# print(doubled_dict)
+# print(len(doubled_dict))
+
+
+#Task 4
+
+# string = "today is a sunny day"
+# count_u = string.count('u')
+# count_i = string.count('i')
+# total_count = count_u + count_i
+# print(total_count)
+
+
+#Task 5
+
+# lst = ['hello', 'today', 'is', 'too', 'cold']
+# filtered_list = [word for word in lst if len(word) > 3]
+# print(filtered_list)
